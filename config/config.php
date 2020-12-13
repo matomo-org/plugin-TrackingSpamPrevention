50d13fe27aaa51bdf3e788e795fc091cff877206
Thomas Steur <tsteur@users.noreply.github.com>
2020-12-13 20:47:30 +1300

    better di definition

--- config/config.php
+++ config/config.php
@@ -4,18 +4,18 @@ return array(
     'Piwik\Plugins\TrackingSpamPrevention\BlockedIpRanges' => DI\autowire()
         ->constructor(DI\get('trackingspam.iprangeproviders')),
 
-    'trackingspam.iprangeproviders' => array(
+    'trackingspam.iprangeproviders' => DI\add(array(
         DI\get('Piwik\Plugins\TrackingSpamPrevention\BlockedIpRanges\Aws'),
         DI\get('Piwik\Plugins\TrackingSpamPrevention\BlockedIpRanges\Azure'),
         DI\get('Piwik\Plugins\TrackingSpamPrevention\BlockedIpRanges\DigitalOcean'),
         DI\get('Piwik\Plugins\TrackingSpamPrevention\BlockedIpRanges\Gcloud'),
         DI\get('Piwik\Plugins\TrackingSpamPrevention\BlockedIpRanges\Oracle'),
-    ),
+    )),
 
     'Piwik\Plugins\TrackingSpamPrevention\BlockedGeoIp' => DI\autowire()
         ->constructor(DI\get('trackingspam.geoipmatchproviders')),
 
-    'trackingspam.geoipmatchproviders' => array(
+    'trackingspam.geoipmatchproviders' => DI\add(array(
         'alicloud', 'alibaba cloud'
-    )
+    ))
 );
