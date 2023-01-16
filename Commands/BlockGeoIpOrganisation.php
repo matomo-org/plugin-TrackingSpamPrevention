daf05c391c132b9e7b874c38212c402dfac09395
Thomas Steur <tsteur@users.noreply.github.com>
2023-01-16 13:12:51 +1300

    minor tweak

--- Commands/BlockGeoIpOrganisation.php
+++ Commands/BlockGeoIpOrganisation.php
@@ -34,7 +34,10 @@ class BlockGeoIpOrganisation extends ConsoleCommand
         if (empty($pluginConfig[Configuration::KEY_GEOIP_MATCH_PROVIDERS])) {
             $pluginConfig[Configuration::KEY_GEOIP_MATCH_PROVIDERS] = [];
         }
-        $pluginConfig[Configuration::KEY_GEOIP_MATCH_PROVIDERS][] = mb_strtolower(trim($input->getOption('organisation-name')));
+
+        $name = $input->getOption('organisation-name');
+        $pluginConfig[Configuration::KEY_GEOIP_MATCH_PROVIDERS][] = mb_strtolower(trim($name));
+        
         $pluginConfig[Configuration::KEY_GEOIP_MATCH_PROVIDERS] = array_values(array_unique($pluginConfig[Configuration::KEY_GEOIP_MATCH_PROVIDERS]));
 
         $config->TrackingSpamPrevention = $pluginConfig;
