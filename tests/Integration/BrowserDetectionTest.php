c56baf52be398f0ce7c7040f6eedebb67cc57b58
Jacob Ransom <jacob@innocraft.com>
2023-02-16 13:26:48 +1300

    Making some test improvements

--- tests/Integration/BrowserDetectionTest.php
+++ tests/Integration/BrowserDetectionTest.php
@@ -10,6 +10,7 @@ namespace Piwik\Plugins\TrackingSpamPrevention\tests\Integration;
 
 use Piwik\Plugins\TrackingSpamPrevention\BrowserDetection;
 use Piwik\Tests\Framework\TestCase\IntegrationTestCase;
+use Piwik\Version;
 
 /**
  * @group TrackingSpamPrevention
@@ -59,13 +60,14 @@ class BrowserDetectionTest extends IntegrationTestCase
 
     public function getServerSideLibraries()
     {
+        $isMatomoPhp8Compatible = version_compare(Version::VERSION, '4.7.0-b1', '>=') || version_compare(PHP_VERSION, '8.0.0', '<');
         return [
             [false, ''],
             [false, null],
             [false, 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'],
             [false, 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0'],
-            [true, 'curl/7.68.0'],
-            [true, 'Wget/1.20.3 (linux-gnu)'],
+            [$isMatomoPhp8Compatible, 'curl/7.68.0'],
+            [$isMatomoPhp8Compatible, 'Wget/1.20.3 (linux-gnu)'],
         ];
     }
 }
--- tests/Integration/TrackingSpamPreventionTest.php
+++ tests/Integration/TrackingSpamPreventionTest.php
@@ -18,6 +18,7 @@ use Piwik\Tests\Framework\TestCase\IntegrationTestCase;
 use Piwik\Tracker\Cache;
 use Piwik\Tracker\Request;
 use Piwik\Tracker\VisitExcluded;
+use Piwik\Version;
 
 /**
  * @group TrackingSpamPrevention
@@ -113,7 +114,11 @@ class TrackingSpamPreventionTest extends IntegrationTestCase
         $excluded = $this->makeExcluded('22.22.22.22');
         $isExcluded = $excluded->isExcluded();
         unset($_SERVER['HTTP_USER_AGENT']);
-        $this->assertFalse($isExcluded);
+        if (version_compare(Version::VERSION, '4.7.0-b1', '>=') || version_compare(PHP_VERSION, '8.0.0', '<')) {
+            $this->assertFalse($isExcluded);
+        } else {
+            $this->assertTrue($isExcluded);
+        }
     }
 
     public function test_isExcludedVisit_whenBlockServerSideLibraryEnabledAndNotServerSideUserAgent() {
